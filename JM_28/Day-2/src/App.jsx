ff1f9dca7a8bb09fce884a3976076f8cec9a5f69
jagamohan01 <jagamohanmishra143@gmail.com>
2026-01-10 11:55:44 +0530

    Day-3 updated

--- JM_28/Day-2/src/App.jsx
+++ JM_28/Day-2/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [message, setMessage] = useState("");
+
+  // Runs once when component loads (API fetch)
+  useEffect(() => {
+    fetch("https://api.chucknorris.io/jokes/random")
+      .then(res => res.json())
+      .then(data => setMessage(data.value));
+  }, []);
+
+  // Runs every time count changes
+  useEffect(() => {
+    console.log("Button clicked", count, "times");
+  }, [count]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div style={{ padding: "20px" }}>
+      <h1>Click Counter (Level 2 - Hooks)</h1>
+
+      <h2>Count: {count}</h2>
+      <button onClick={() => setCount(count + 1)}>
+        Click Me
+      </button>
+
+      <hr />
+    </div>
+  );
 }
 
-export default App
+export default App;
