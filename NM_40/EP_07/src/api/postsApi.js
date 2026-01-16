d76ead69d6b5699baec7497a0e41b680ca1e5682
Nikhil Maharana <nikhilmaharana72@gmail.com>
2026-01-16 14:52:35 +0530

    Day 7

---
+++ NM_40/EP_07/src/api/postsApi.js
@@ -0,0 +1,8 @@
+export const fetchPosts = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    if (!res.ok) {
+      throw new Error("Failed to fetch posts");
+    }
+    return res.json();
+  };
+  
