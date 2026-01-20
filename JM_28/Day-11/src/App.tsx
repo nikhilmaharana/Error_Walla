133a65b778ce3fdd4dfcce6a28239933ac3d4113
jagamohan01 <jagamohanmishra143@gmail.com>
2026-01-20 15:29:44 +0530

    Day-11 added

---
+++ JM_28/Day-11/src/App.tsx
@@ -0,0 +1,12 @@
+import Greeting from "./components/Greeting"
+
+function App() {
+  return (
+    <div>
+      <h1>Hello there.</h1>
+      <Greeting name="Jagamohan" age={23} />
+    </div>
+  )
+}
+
+export default App
